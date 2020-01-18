1599a4c2460a5fbd72b8a7d623afeaa1836cdafc
kakao <wodlf30426@gmail.com>
2020-01-19 00:24:03 +0900

    Link 컴포넌트 사용
    Link 컴포넌트는 클릭하면 다른 주소로 이동시켜 주는 컴포넌트
    a 태그를 쓰면 페이지를 새로 불러오기 때문에 애플리케이션이 들고 있던 상태를 날린다.
    Link 컴포넌트를 사용하면 애플리케이션은 그대로 유지한 상태에서 HTML5 History API를 사용하여 페이지의 주소만 변경해준다

--- prac13_react_router/router-tutorial/src/App.js
+++ prac13_react_router/router-tutorial/src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 
+// Route 컴포넌트는 특정 주소에 컴포넌트를 연결시켜 준다
+// Link 컴포넌트는 클릭하면 다른 주소로 이동시켜 주는 컴포넌트
 function App() {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
     </div>
