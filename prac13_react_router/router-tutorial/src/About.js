79ca33c421d6964319a0ed99ab26e71ddd17a33d
kakao <wodlf30426@gmail.com>
2020-01-19 01:04:27 +0900

    url 쿼리 실습
    url 쿼리를 받아오기 위해서 컴포넌트에 location 객체를 전달받음
    쿼리 문자열을 객체로 변환하기 위해서 qs 라이브러리 사용
    쿼리 파싱 결과는 언제나 문자열임을 주의

--- prac13_react_router/router-tutorial/src/About.js
+++ prac13_react_router/router-tutorial/src/About.js
@@ -1,10 +1,18 @@
 import React from "react";
+import qs from "qs";
 
-const About = () => {
+// url 쿼리 받아올 때는 location
+// url 파라미터 받아올 때는 match
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true
+  });
+  const showDetail = query.detail === "true";
   return (
     <div>
       <h1>소개</h1>
       <p>예제임</p>
+      {showDetail && <p>detail 값을 true로 설정했누</p>}
     </div>
   );
 };
