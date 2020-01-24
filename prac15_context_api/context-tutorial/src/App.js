f79ef0403af0bfb33b66a8d53cea56889c05033d
kakao <wodlf30426@gmail.com>
2020-01-24 16:03:43 +0900

    Context API 실습
    createContext함수를 사용하여 Context 생성
    Provider를 사용하여 Context의 value 변경
    Consumer를 사용하여 Context의 value 사용
    value에 함수를 전달하여 동적 context 사용

---
+++ prac15_context_api/context-tutorial/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ColorBox from "./contexts/ColorBox";
+import { ColorProvider } from "./contexts/color";
+
+function App() {
+  return (
+    <ColorProvider>
+      <div>
+        <ColorBox />
+      </div>
+    </ColorProvider>
+  );
+}
+
+export default App;
---
+++ prac15_context_api/context-tutorial/src/contexts/color.js
@@ -0,0 +1,29 @@
+import React, { createContext, useState } from "react";
+
+//default 값은 Provider를 사용하지 않을 때 적용될 값
+const ColorContext = createContext({
+  state: { color: "black", subcolor: "red" },
+  action: {
+    setColor: () => {},
+    setSubcolor: () => {}
+  }
+});
+
+const ColorProvider = ({ children }) => {
+  const [color, setColor] = useState("black");
+  const [subcolor, setSubcolor] = useState("red");
+
+  const value = {
+    state: { color, subcolor },
+    action: { setColor, setSubcolor }
+  };
+  //Context의 value에 함수를 전달해 줄 수도 있음
+  return (
+    <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+  );
+};
+
+const { Consumer: ColorConsumer } = ColorContext;
+export { ColorProvider, ColorConsumer };
+
+export default ColorContext;
