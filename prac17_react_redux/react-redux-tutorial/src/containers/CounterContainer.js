621e8fe24306342d98aadc5a02785f907eb56701
kakao <wodlf30426@gmail.com>
2020-01-25 18:16:26 +0900

    Hooks를 사용하여 컨테이너 컴포넌트 만들기
    useSelector
    useDispatch
    useStore
    connect와 다르게 부모 컴포넌트 리렌더링 시 해당 컴포넌트의 props 가 바뀌지 않았다면 리렌더링이 자동으로 방지되지 않음

--- prac17_react_redux/react-redux-tutorial/src/containers/CounterContainer.js
+++ prac17_react_redux/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Counter from '../components/Counter';
-import { connect } from 'react-redux';
-// connect(mapStateToProps, mapDispatchToProps)(연동할 컴포넌트)
-// 컴포넌트를 리덕스와 연동하려면 connect 함수 사용
-// mapStateToProps : 리덕스 스토어 안의 상태를 컴포넌트의 props로 넘겨주기 위해 설정하는 함수
-// mapDispatchToProps : 액션 생성 함수를 컴포넌트의 props로 넘겨주기 위해 사용하는 함수
+import { useSelector, useDispatch } from 'react-redux';
+// const 결과 = useSelector(상태 선택 함수)
 import { increase, decrease } from '../modules/Counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector(state => state.counter.number);
+  const dispatch = useDispatch();
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter
+      number={number}
+      onIncrease={useCallback(() => dispatch(increase()), [dispatch])}
+      onDecrease={useCallback(() => dispatch(decrease()), [dispatch])}
+    />
   );
 };
 
-export default connect(
-  state => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  },
-)(CounterContainer);
+export default CounterContainer;
