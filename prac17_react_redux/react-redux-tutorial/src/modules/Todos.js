544842f5e029e67653601f95bed0d5f75fcf6599
kakao <wodlf30426@gmail.com>
2020-01-25 16:05:55 +0900

    redux-actions 실습
    redux-actoins의 createAction, handleActions 사용
    createAction : action 생성 함수
    handleActions : reducer 함수
    handleActions에 파라미터를 넣게 되면 payload라는 이름을 사용해 가져온다

--- prac17_react_redux/react-redux-tutorial/src/modules/Todos.js
+++ prac17_react_redux/react-redux-tutorial/src/modules/Todos.js
@@ -1,3 +1,5 @@
+import { createAction, handleActions } from 'redux-actions';
+
 //액션 타입
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
@@ -5,18 +7,15 @@ const TOGGLE = 'todos/TOGGLE';
 const REMOVE = 'todos/REMOVE';
 
 //액션 생성 함수
-export const changeInput = input => ({ type: CHANGE_INPUT, input });
+export const changeInput = createAction(CHANGE_INPUT, input => input);
 let id = 3;
-export const insert = text => ({
-  type: INSERT,
-  todo: {
-    id: id++,
-    text,
-    done: false,
-  },
-});
-export const toggle = id => ({ type: TOGGLE, id });
-export const remove = id => ({ type: REMOVE, id });
+export const insert = createAction(INSERT, text => ({
+  id: id++,
+  text,
+  done: false,
+}));
+export const toggle = createAction(TOGGLE, id => id);
+export const remove = createAction(REMOVE, id => id);
 
 //초기 상태
 const initialState = {
@@ -36,34 +35,25 @@ const initialState = {
 };
 
 //리덕스 함수
-function todos(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_INPUT:
-      return {
-        ...state,
-        input: action.input,
-      };
-    case INSERT:
-      return {
-        ...state,
-        todos: [...state.todos, action.todo],
-      };
-    case TOGGLE:
-      return {
-        ...state,
-        todos: state.todos.map(todo =>
-          todo.id === action.id ? { ...todo, done: !todo.done } : todo,
-        ),
-      };
-    case REMOVE:
-      return {
-        ...state,
-        todos: state.todos.filter(todo => todo.id !== action.id),
-      };
-
-    default:
-      return state;
-  }
-}
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+    [INSERT]: (state, { payload: todo }) => ({
+      ...state,
+      todos: [...state.todos, todo],
+    }),
+    [TOGGLE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.map(todo =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo,
+      ),
+    }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter(todo => todo.id !== id),
+    }),
+  },
+  initialState,
+);
 
 export default todos;
